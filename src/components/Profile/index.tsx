24103de344c98a9971f8629565b1fb956d666ffd
João Gonçalves <joaogoncalves@MacBook-Air-de-Joao.local>
2022-12-08 17:39:18 -0300

    imgs-projects

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,5 +1,5 @@
-import * as C from './styles'
-
+import * as C from './styles';
+import project1 from '../../imgs/project1.png';
 
 export const Projects = ( )=> {
     return (
@@ -8,15 +8,15 @@ export const Projects = ( )=> {
                 <h1>Aqui estão meus melhores projetos</h1>
 
                 <a href="https://github.com/Kenzie-Academy-Brasil-Developers/api-kenziefood-m2-lara-plc">
-                    <img src="" alt="" />
+                    <img src={ project1 } alt=""></img>
                 </a>
 
                 <a href="https://www.w3schools.com">
-                    <img src="" alt="" />
+                    <img src={ project1 } alt=""></img>
                 </a>
 
                 <a href="https://www.w3schools.com">
-                    <img src="" alt="" />
+                    <img src={ project1 } alt=""></img>
                 </a>
             </div>
         </C.Container>
--- src/components/Profile/styles.ts
+++ src/components/Profile/styles.ts
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-
+img{
+    width: 200px;
+    height: 200px;
+}
 
 `
