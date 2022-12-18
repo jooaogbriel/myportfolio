03c49c70c27cee0736053be1a48eaa6f2f91f780
João Gonçalves <joao_i0807@outlook.com>
2022-12-18 10:18:40 -0300

    styles

--- src/components/Dash/index.tsx
+++ src/components/Dash/index.tsx
@@ -18,22 +18,23 @@ export const Home = ( )=> {
                     </ul>
                 </nav>
                 <div>
-                <h1>Aqui estão meus melhores projetos</h1>
-
-                <a href="https://github.com/Kenzie-Academy-Brasil-Developers/api-kenziefood-m2-lara-plc">
-                    
-                </a>
-
-                <a href="https://www.w3schools.com">
-                    
-                </a>
-
-                <a href="https://www.w3schools.com">
-                    
-                </a>
-                <a href="https://www.w3schools.com">
-                    
-                </a>
+                <h2>Hello Word!</h2>
+                <h1>Me chamo João Gabriel </h1>
+                <h1>Desenvolvedor Full Stack com 1 ano de experiência </h1>
+            </div><div>
+                <h2>Hello Word!</h2>
+                <h1>Me chamo João Gabriel </h1>
+                <h1>Desenvolvedor Full Stack com 1 ano de experiência </h1>
+            </div>
+            <div>
+                <h2>Hello Word!</h2>
+                <h1>Me chamo João Gabriel </h1>
+                <h1>Desenvolvedor Full Stack com 1 ano de experiência </h1>
+            </div>
+            <div>
+                <h2>Hello Word!</h2>
+                <h1>Me chamo João Gabriel </h1>
+                <h1>Desenvolvedor Full Stack com 1 ano de experiência </h1>
             </div>
                 </div>
                 
--- src/components/Dash/styles.ts
+++ src/components/Dash/styles.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-
+    width: 200px;
+    height: 200px;
 
 `
