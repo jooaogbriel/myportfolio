a4471f51f41ebff8dec6a24ef7c29918736cd730
João Gonçalves <joao_i0807@outlook.com>
2023-02-05 09:41:43 -0300

    aplicacao concluida

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -48,7 +48,6 @@ export const Projects = ( )=> {
                     <nav>
                         <span>ReactJS</span>
                         <span>Yup</span>
-                        <span>Yup</span>
                         <span>React Hook Form</span>
                     </nav>
                 </div>
