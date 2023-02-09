1e7589eb9cb65da07a5901d3243b8dd3ec662ae6
João Gonçalves <joao_i0807@outlook.com>
2023-02-09 15:00:56 -0300

    fix: resolv bugs imgs background

--- src/components/Dash/index.tsx
+++ src/components/Dash/index.tsx
@@ -24,6 +24,9 @@ export const Home = ( )=> {
             <img className='coding' src={coding} alt=''></img>
             <img  className='dino' src={dino} alt=''></img>
             <img  className='html' src={html} alt=''></img>
+            <img className='coding' src={coding} alt=''></img>
+            <img  className='dino' src={dino} alt=''></img>
+            <img  className='html' src={html} alt=''></img>
             <img  className='js' src={jsback} alt=''></img>
 
             <div className='list'>
